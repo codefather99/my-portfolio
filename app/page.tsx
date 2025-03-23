810e46921e789ad0b5b36b39cdf5071ffe6322da
codefather99 <emefokamsi@gmail.com>
2025-03-23 15:13:10 +0100

    Initial Setup

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+
+export default function Home() {
+  return (
+    <section className="py-2 px-2">
+      <div className="container">
+        <h1 className="text-3xl font-bold">
+          Get Started
+        </h1>
+
+      </div>
+    </section>
+  )
+}
