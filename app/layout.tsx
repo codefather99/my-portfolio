c55aab613c4c112291a16161b0ff62e108fae28e
codefather99 <emefokamsi@gmail.com>
2025-05-01 11:48:32 +0100

    deploy 02

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,16 +3,16 @@ import { Geist, Geist_Mono, Inter, Space_Grotesk } from "next/font/google";
 import "./globals.css";
 import LayoutWrapper from "@/components/layoutWrapper";
 
-const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
+const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"], display: "swap" });
 const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"], display: "swap" });
 const inter = Inter({ variable: "--font-inter", subsets: ["latin"], display: "swap" });
-const spaceGrotesk = Space_Grotesk({ variable: "--font-space-grotesk", subsets: ["latin"] });
+const spaceGrotesk = Space_Grotesk({ variable: "--font-space-grotesk", subsets: ["latin"], display: "swap" });
 
 export const metadata: Metadata = {
   title: "Emefo",
   description: "Emefo Henry's Portfolio",
   icons: {
-    icon: "/favicon.png", // or "/favicon.png"
+    icon: "/favicon.png",
   },
 };
 
@@ -25,7 +25,6 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`font-space-grotesk antialiased ${geistSans.variable} ${geistMono.variable} ${inter.variable} ${spaceGrotesk.variable}`}
-        style={{ fontFamily: "var(--font-space-grotesk)" }}
       >
         <LayoutWrapper>{children}</LayoutWrapper>
       </body>
